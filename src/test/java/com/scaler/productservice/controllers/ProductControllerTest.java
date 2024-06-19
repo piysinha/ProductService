87504a3e57d35841ee3cf9998d8e9bbe9f3c6bf3
piysinha <piyushdinkarsinha95@gmail.com>
2024-06-19 21:00:50 +0530

    Implementing Assert J test cases

--- src/test/java/com/scaler/productservice/controllers/ProductControllerTest.java
+++ src/test/java/com/scaler/productservice/controllers/ProductControllerTest.java
@@ -13,6 +13,8 @@ import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
+import static org.assertj.core.api.Assertions.*;
+
 @SpringBootTest
 class ProductControllerTest {
     @Autowired
@@ -30,9 +32,17 @@ class ProductControllerTest {
 
         when(productService.getAllProducts()).thenReturn(p);
         List<Product> products = productController.getAllProducts();
-        assertEquals(109.95, products.get(0).getPrice());
+//        assertEquals(109.95, products.get(0).getPrice());
         //assertTrue(products.get(0).getPrice() == 109.95); //Returning Boolean
         //assert products.get(0).getPrice() == 109.95; //Normal Assert does not show values when failed
+
+        //1. Using the AssertJ libraries to carry the tests in more readable manner.
+        // a. We will first pick the date that we want to validate.
+        // b. Call the different validations that you want to do.
+        assertThat(products.get(0).getPrice())
+                .isEqualTo(109.95)
+                .isGreaterThan(100)
+                .isLessThan(100);
     }
 
 }
