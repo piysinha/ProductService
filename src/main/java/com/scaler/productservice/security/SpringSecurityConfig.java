dfef5ef6472a2e8a31f49e0b3841ef6cc2a68d81
piysinha <piyushdinkarsinha95@gmail.com>
2024-07-27 17:41:07 +0530

    Adding environment variables

--- src/main/java/com/scaler/productservice/security/SpringSecurityConfig.java
+++ src/main/java/com/scaler/productservice/security/SpringSecurityConfig.java
@@ -14,7 +14,7 @@ public class SpringSecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests(authorize -> authorize
-                        .requestMatchers("/products").hasAuthority("ADMIN_ROLE")
+                        .requestMatchers("/products").hasAuthority("ADMIN")
                         .anyRequest().permitAll()
                 )
                 .oauth2ResourceServer((oauth2) -> oauth2.jwt(Customizer.withDefaults()))
